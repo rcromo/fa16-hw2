e1646b96380a630dccc65077568f06d8d0bf4881
Roberto Romo <rromo@berkeley.edu>
2016-09-26 09:18:26 -0700

    fixed and finished

--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -4,11 +4,12 @@ class Person
   def initialize(name, age)
     @name = name
     @age = age
+    @nickname = @name[0, 4]
   end
 
   def nickname
     # YOUR IMPLEMENTATION HERE
-    return @name[0, 4]
+    return @@nickname
   end
 
   def birth_year
